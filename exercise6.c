c12ddd594a8caf14e73449a076195011b561e65b
decampus <moiseswilliam.cs@gmail.com>
2024-03-19 20:59:31 -0300

    Exercise 6

---
+++ exercise6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int radius;
+    float circle_perimeter, circle_area;
+
+    // Assign a value to the radius.
+    radius = 6;
+
+    // Calculating the perimeter of the circle.
+    circle_perimeter = 2 * M_PI * radius;
+
+    // Calculating the area of the circle.
+    circle_area = M_PI * radius * radius;
+
+    // Print the perimeter of the circle.
+    printf("Perimeter of the circle: %f\n", circle_perimeter);
+    // Print the area of the circle.
+    printf("Area of the circle: %f\n", circle_area);
+
+    
+    return 0;
+}
