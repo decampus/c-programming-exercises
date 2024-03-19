50e7319b5828bb8f37a47bc4b6ff276346dc755e
decampus <moiseswilliam.cs@gmail.com>
2024-03-18 23:21:53 -0300

    Exercise 3

---
+++ exercise3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // Print "F".
+    printf("#####\n"
+           "#\n"
+           "#\n"
+           "####\n"
+           "#\n"
+           "#\n"
+           "#\n");
+    // Print "C".
+    printf("   ######\n"
+           " ##      ##\n"
+           "#\n"
+           "#\n"
+           "#\n"
+           "#\n"
+           "#\n"
+           " ##       ##\n"
+           "   ######\n");
+           
+    return 0;
+}
