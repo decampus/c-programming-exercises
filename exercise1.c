2d97c8cb1dcd33608da9c914714b1d1e609dbbe6
decampus <moiseswilliam.cs@gmail.com>
2024-03-18 22:39:16 -0300

    Exercise 1

---
+++ exercise1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Print name.
+    printf("Name    : William Albuquerque\n");
+    // Print date of birth.
+    printf("DOB     : September 23, 2002\n");
+    // Print mobile number.
+    printf("Mobile  : 99-9999999999\n");
+
+    return 0;
+}
